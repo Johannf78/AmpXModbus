62f464f10f6f440da75aef4302704ce43fb5cfa4
Johann Fritz <mr.johann.temp@gmail.com>
2024-11-29 09:27:14 +0100

    Small changes to wifi

--- AmpXModbus/ampx_open_energy_gateway/ampx_open_energy_gateway.ino
+++ AmpXModbus/ampx_open_energy_gateway/ampx_open_energy_gateway.ino
@@ -91,13 +91,17 @@ void initWiFi() {
   // Connect to the Wi-Fi network
   WiFi.begin(ssid, password);
 
+  Serial.print("Now connecting to WiFi...");
   while (WiFi.status() != WL_CONNECTED) {
     delay(1000);
-    Serial.println("Now connecting to WiFi...");
+    Serial.print(".");//Print more dots while connecting 
   }
+  Serial.println("");
   Serial.println("Connected to Wi-Fi");
   Serial.print("IP address: ");
   Serial.println(WiFi.localIP());
+  Serial.print("Signal strength: ");
+  Serial.println(WiFi.RSSI());
 }
 
 void initServer() {
