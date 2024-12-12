2e46c8ea7599b8f8490852dd1dc3a9235f477074
Johann Fritz <mr.johann.temp@gmail.com>
2024-12-12 08:41:14 +0200

    added multiple meters

--- AmpXModbus/ampx_open_energy_gateway/webpage.h
+++ AmpXModbus/ampx_open_energy_gateway/webpage.h
@@ -1,5 +1,5 @@
 //Raw string literal quotation, webpage HTML...
-String webpage = R"(
+//String webpage = R"(
 <!DOCTYPE html>
 <html>
 <head>
@@ -17,10 +17,11 @@ String webpage = R"(
   </style>
 </head>
 <body>
-  <main>
-    <h1>AmpX Open Energy Gateway</h1>
-    
-    <br/>
+<main>
+  <h1>AmpX Open Energy Gateway</h1>
+  
+  <br/>
+  <div id='meter1'>
     <h3>Meter: 1, Serial number: m1_serial_number</h3>
     Test: <span id='test'>--</span>
     <table>
@@ -33,38 +34,84 @@ String webpage = R"(
     </tr>
     <tr>
       <td class='text-align-left'>Voltage</td>
-      <td id='voltage_L1'>0.00 V</td>
-      <td id='voltage_L2'>0.00 V</td>
-      <td id='voltage_L3'>0.00 V</td>
+      <td id='m1_voltage_L1'>0.00 V</td>
+      <td id='m1_voltage_L2'>0.00 V</td>
+      <td id='m1_voltage_L3'>0.00 V</td>
       <td></td>
     </tr>
     <tr>
       <td class='text-align-left'>Current</td>
-      <td id='current_L1'>0.00 A</td>
-      <td id='current_L2'>0.00 A</td>
-      <td id='current_L3'>0.00 A</td>
-      <td id='current_avg'>0.00 A</td>
+      <td id='m1_current_L1'>0.00 A</td>
+      <td id='m1_current_L2'>0.00 A</td>
+      <td id='m1_current_L3'>0.00 A</td>
+      <td id='m1_current_avg'>0.00 A</td>
     </tr>
     <tr>
       <td class='text-align-left'>Active Power</td>
-      <td id='active_power_L1'>0.00 KW</td>
-      <td id='active_power_L2'>0.00 KW</td>
-      <td id='active_power_L3'>0.00 KW</td>
-      <td id='active_power_tot'>0.00 KW</td>
+      <td id='m1_active_power_L1'>0.00 KW</td>
+      <td id='m1_active_power_L2'>0.00 KW</td>
+      <td id='m1_active_power_L3'>0.00 KW</td>
+      <td id='m1_active_power_tot'>0.00 KW</td>
     </tr>
     <tr>
       <td class='text-align-left'>Active Energy Imported</td>
-      <td id='active_energy_imported_L1'>0 kWh</td>
-      <td id='active_energy_imported_L2'>0 kWh</td>
-      <td id='active_energy_imported_L3'>0 kWh</td>
-      <td id='active_energy_imported_tot'>0 kWh</td>
+      <td id='m1_active_energy_imported_L1'>0 kWh</td>
+      <td id='m1_active_energy_imported_L2'>0 kWh</td>
+      <td id='m1_active_energy_imported_L3'>0 kWh</td>
+      <td id='m1_active_energy_imported_tot'>0 kWh</td>
     </tr>
     </table>
-  </main>
-  <footer>
-  <br/><br/>
-  <span><a href='www.ampx.co/' target'_blank'>www.ampx.co</a></span>
-  </footer>
+  </div>
+
+  <br/>
+  <div id='meter2'>
+    <h3>Meter: 2, Serial number: m2_serial_number</h3>
+    Test: <span id='test'>--</span>
+    <table>
+    <tr>
+      <th>Entity</th>
+      <th>L1</th>
+      <th>L2</<th>
+      <th>L3</<th>
+      <th>Avg/Max</<th>
+    </tr>
+    <tr>
+      <td class='text-align-left'>Voltage</td>
+      <td id='m2_voltage_L1'>0.00 V</td>
+      <td id='m2_voltage_L2'>0.00 V</td>
+      <td id='m2_voltage_L3'>0.00 V</td>
+      <td></td>
+    </tr>
+    <tr>
+      <td class='text-align-left'>Current</td>
+      <td id='m2_current_L1'>0.00 A</td>
+      <td id='m2_current_L2'>0.00 A</td>
+      <td id='m2_current_L3'>0.00 A</td>
+      <td id='m2_current_avg'>0.00 A</td>
+    </tr>
+    <tr>
+      <td class='text-align-left'>Active Power</td>
+      <td id='m2_active_power_L1'>0.00 KW</td>
+      <td id='m2_active_power_L2'>0.00 KW</td>
+      <td id='m2_active_power_L3'>0.00 KW</td>
+      <td id='m2_active_power_tot'>0.00 KW</td>
+    </tr>
+    <tr>
+      <td class='text-align-left'>Active Energy Imported</td>
+      <td id='m2_active_energy_imported_L1'>0 kWh</td>
+      <td id='m2_active_energy_imported_L2'>0 kWh</td>
+      <td id='m2_active_energy_imported_L3'>0 kWh</td>
+      <td id='m2_active_energy_imported_tot'>0 kWh</td>
+    </tr>
+    </table>
+  </div>
+
+
+</main>
+<footer>
+<br/><br/>
+<span><a href='www.ampx.co/' target'_blank'>www.ampx.co</a></span>
+</footer>
 </body>
 <script>
   var socket;
